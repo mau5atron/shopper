59423039c08f951561da26a013f47ac0e4eedb0e
mau5atron <gabrielbetancourt96@outlook.com>
2018-09-06 11:53:22 -0400

    added associates / remove price from categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_04_042324) do
+ActiveRecord::Schema.define(version: 2018_09_06_155122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "categories", force: :cascade do |t|
     t.string "title", null: false
-    t.decimal "price", precision: 15, scale: 2, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
