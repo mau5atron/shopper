fd594205dc7ec61f3fadbc7ca183bef8621322e3
mau5atron <gabrielbetancourt96@outlook.com>
2018-09-08 22:41:54 -0400

    restructured db schema / added current cart updating + secure updating / made cart available accessible to other controllers + home && cart links in views / added sub total to cart view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::Base
+	protect_from_forgery with: :exception 
+
+	# call before action on currrent cart in order for:
+		# current_cart to be created as soon as first request is made
+	before_action :current_cart
+
+	# current cart is available everywhere in the application since every controller
+	# inherits from application controller 
+	def current_cart
+		@current_cart ||= ShoppingCart.new(token: cart_token)
+	end
+
+	# makes current_cart method available in the views
+	helper_method :current_cart
+
+	private 
+
+	def cart_token
+		# return cart token if it is not nil 
+		return @cart_token unless @cart_token.nil?
+
+		# if cart session exists then continue, 
+ 		# if never had a cart token before then we define cart token as a new hash 
+		session[:cart_token] ||= SecureRandom.hex(8)
+		# take session cart token and assign to instance variable 
+		# cart token to make it accessible 	
+		@cart_token = session[:cart_token]
+	end
 end
---
+++ app/models/shopping_cart.rb
@@ -0,0 +1,68 @@
+class ShoppingCart
+	
+	# delegate makess sub_total public to specified classes/models
+	# when calling subtotal from order, will be able to call sub_total with: current_cart.sub_total
+	delegate :sub_total, to: :order 
+
+	# shopping cart will hold all data for our shopping cart
+	def initialize(token:)
+		# when initalizing shopping cart, will figure out who's shopping cart it belongs to 
+		@token = token
+	end
+
+	def order
+		# if @order instance has been assigned in the past, it will return that instance
+		# otherwise a new order token instance will be created
+		@order ||= Order.find_or_create_by(token: @token) do |order|
+			order.sub_total = 0
+		end
+	end
+
+	def items_count
+		order.items.sum(:quantity)		
+	end
+
+	# will be used in the order items controller 
+	def add_item(product_id:, quantity: 1)
+		# find product based on product id 
+		product = Product.find(product_id)
+
+		# find or create a an order item for that order id
+		# find order item based on current order
+		order_item = order.items.find_or_initialize_by(
+			product_id: product_id
+		)
+
+		order_item.price = product.price
+		order_item.quantity = quantity
+
+		# save order_item
+
+		# TRANSACTION
+		# to avoid having either method not saving 
+		# wrap in a transaction block to ensure SQL statements are permanent if 
+		# can succeed as one action
+		ActiveRecord::Base.transaction do 
+			order_item.save
+			update_sub_total!
+		end
+		# TRANSACTION
+	end
+
+	def remove_item(id:)
+		# TRANSACTION
+		ActiveRecord::Base.transaction do 
+			order.items.destroy(id)
+			update_sub_total!
+		end
+		# TRANSACTION
+	end
+
+	private 
+
+	# update sub_total when updating (removing or adding to current_cart)
+	def update_sub_total!
+		order.sub_total = order.items.sum('quantity * price')
+		order.save
+	end
+end
